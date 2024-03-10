ecb9dc8823f918091945f614946c80f61a0f7598
Zem <82421333+Zemaille@users.noreply.github.com>
2024-03-10 11:06:07 -0400

    Electivire sprite updated

--- gfx/pokemon/electivire/anim_idle.asm
+++ gfx/pokemon/electivire/anim_idle.asm
@@ -1,2 +1,6 @@
-	frame 0, 0
+	setrepeat 2
+	frame 4, 06
+	frame 0, 06
+	dorepeat 1
+	frame 5, 08
 	endanim
--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -191,8 +191,8 @@ TotodilePokeBallScript:
 	iftrue LookAtElmPokeBallScript
 	turnobject ELMSLAB_ELM, DOWN
 	refreshscreen
-	pokepic YANMEGA
-	cry YANMEGA
+	pokepic ELECTIVIRE
+	cry ELECTIVIRE
 	waitbutton
 	closepokepic
 	opentext
@@ -204,12 +204,12 @@ TotodilePokeBallScript:
 	writetext ChoseStarterText
 	promptbutton
 	waitsfx
-	getmonname STRING_BUFFER_3, YANMEGA
+	getmonname STRING_BUFFER_3, ELECTIVIRE
 	writetext ReceivedStarterText
 	playsound SFX_CAUGHT_MON
 	waitsfx
 	promptbutton
-	givepoke YANMEGA, 5, BERRY
+	givepoke ELECTIVIRE, 5, BERRY
 	closetext
 	applymovement PLAYER, AfterTotodileMovement
 	sjump ElmDirectionsScript
