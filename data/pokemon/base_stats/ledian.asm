b1b88f2a2fb6c01021e987fedd8b1731b8c622ff
Zem <82421333+Zemaille@users.noreply.github.com>
2025-03-25 20:20:30 -0400

    Fixed some mon stats

--- data/pokemon/base_stats/ledian.asm
+++ data/pokemon/base_stats/ledian.asm
@@ -1,9 +1,9 @@
 	db LEDIAN ; 166
 
-	db  60,  50,  65,  90,  90, 100
+	db  55,  35,  50,  85,  55, 110
 	;   hp  atk  def  spd  sat  sdf
 
-	db BUG, FIGHTING ; type
+	db BUG, FLYING ; type
 	db 90 ; catch rate
 	db 134 ; base exp
 	db NO_ITEM, NO_ITEM ; items
